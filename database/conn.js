368fa3557cf5d87bbfe9c241a55a5192d4093c2c
prateeksingh132 <kumarpks132@gmail.com>
2026-03-03 21:15:21 -0700

    Added a test route to verify server is running on port 3000, works well so far...

--- database/conn.js
+++ database/conn.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
         ////////////TESTING
-        // console.log('TESTING: database connection established successfully ...');
+        console.log('TESTING: database connection established successfully ...');
         ////////////
     } catch (error) {
         console.error("database connection failed. shutting down process...", error.message);
--- server.js
+++ server.js
@@ -1,40 +1,48 @@
 ////////////////////////////////////////// Imports
 import express from "express";
 import dotenv from "dotenv";
-
+import cors from 'cors';
 
 ///////// Import Logging Middleware
-
+import { logReq } from "./middleware/logger.js";
 
 ///////// Import Error Handling Middleware
-
+import { error404, globalErr } from './middleware/error.js';
 
 ///////// Import routes
 
 
 //////// Import Database
-
+import connectDB from './database/conn.js';
 
 // load env vars from .env file
 dotenv.config();
 
 ////////////////////////////////////////// Setups
-const PORT = 3000;
 const app = express();
+const PORT = process.env.PORT || 3001;
+// connect database
+connectDB();
 
 
 
 //////////////////////////////////////// Middleware
-
+app.use(logReq);
 
 
 
 //////////////////////////////////////// Routes
 
+////////////TESTING
+app.get("/", (req, res) => {
+  res.send("testing read!");
+});
+////////////TESTING
 
 
 //////////////////////////////////////// Error Handling
-
+app.use(error404);
+app.use(globalErr);
 
 
 ////////////////////////////////////////// Listener
